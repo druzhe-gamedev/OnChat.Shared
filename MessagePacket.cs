8783ea22b6f3bcf2a677b56ea18adbab7956801c
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-02 23:00:57 +0300

    inherit IPacket

--- MessagePacket.cs
+++ MessagePacket.cs
@@ -1,9 +1,10 @@
 ﻿using OnChat.Protocol;
+using OnChat.Protocol.Packets;
 
 namespace OnChat.Shared;
 
 [PacketId(PacketId.MessagePacket)]
-public class MessagePacket
+public class MessagePacket : IPacket
 {
     public string UserId { get; set; }
     public string Message { get; set; }
