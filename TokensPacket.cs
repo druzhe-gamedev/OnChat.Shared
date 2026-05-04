cc6ae6d7e3e744c106068b12fd02477e534ed2c4
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-04 12:46:46 +0300

    add tokens packet

---
+++ TokensPacket.cs
@@ -0,0 +1,11 @@
+﻿using OnChat.Protocol;
+using OnChat.Protocol.Packets;
+
+namespace OnChat.Shared;
+
+[PacketId(PacketId.TokensPacket)]
+public class TokensPacket : IPacket
+{
+    public string AccessToken { get; set; }
+    public string RefreshToken { get; set; }
+}
