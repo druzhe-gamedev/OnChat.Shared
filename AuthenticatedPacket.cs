5001c998b3ac1e322fb8c6d465540c8371a333e3
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-04 12:46:11 +0300

    add auth packet

---
+++ AuthenticatedPacket.cs
@@ -0,0 +1,9 @@
+﻿using OnChat.Protocol;
+using OnChat.Protocol.Packets;
+
+namespace OnChat.Shared;
+
+public abstract class AuthenticatedPacket : IPacket
+{
+    public string Token { get; set; }
+}
---
+++ AuthenticationPacket.cs
@@ -0,0 +1,11 @@
+﻿using OnChat.Protocol;
+using OnChat.Protocol.Packets;
+
+namespace OnChat.Shared;
+
+[PacketId(PacketId.AuthenticationPacket)]
+public class AuthenticationPacket : IPacket
+{
+    public string Login { get; set; }
+    public string Message { get; set; }
+}
