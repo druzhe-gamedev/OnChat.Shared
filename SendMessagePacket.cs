1cacab878ce41b5ac977c36fc297ce248ba5ac49
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-04 12:46:40 +0300

    change message packet

---
+++ SendMessagePacket.cs
@@ -0,0 +1,11 @@
+﻿using OnChat.Protocol;
+using OnChat.Protocol.Packets;
+
+namespace OnChat.Shared;
+
+[PacketId(PacketId.SendMessagePacket)]
+public class SendMessagePacket : AuthenticatedPacket
+{
+    public string ReceiverId { get; set; }
+    public string Message { get; set; }
+}
