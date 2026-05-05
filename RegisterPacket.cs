04e27ec0758fa1aff8f488dec0c68c76e2438c1b
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-05 17:19:31 +0300

    change RegisterPacket

--- RegisterPacket.cs
+++ RegisterPacket.cs
@@ -6,7 +6,8 @@ namespace OnChat.Shared;
 [PacketId(PacketId.RegistrationPacket)]
 public class RegistrationPacket : BasePacket
 {
-    public string Nickname { get; set; }
-    public string Login { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
     public string Password { get; set; }
+    public short Age { get; set; }
 }
