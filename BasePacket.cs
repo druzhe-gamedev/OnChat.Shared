f64cea3dd47719e3343b5aa8823ae813575c86a3
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-07 00:54:18 +0300

    change string type to nullable for Description parameter

--- BasePacket.cs
+++ BasePacket.cs
@@ -7,14 +7,14 @@ public abstract record BasePacket(Guid CorrelationId) : IPacket
     public Guid CorrelationId { get; set; } = CorrelationId;
 }
 
-public abstract record Response(Guid CorrelationId, string Description) : BasePacket(CorrelationId);
+public abstract record Response(Guid CorrelationId, string? Description = "") : BasePacket(CorrelationId);
 
-public abstract record SuccessfulResponse(Guid CorrelationId, string Description) : Response(CorrelationId, Description), ISuccessfulResponse
+public abstract record SuccessfulResponse(Guid CorrelationId, string? Description = "") : Response(CorrelationId, Description), ISuccessfulResponse
 {
-    public string Description { get; set; } = Description;
+    public string? Description { get; set; } = Description;
 }
 
-public abstract record FailureResponse(Guid CorrelationId, string Description) : Response(CorrelationId, Description), IFailureResponse
+public abstract record FailureResponse(Guid CorrelationId, string? Description = "") : Response(CorrelationId, Description), IFailureResponse
 {
-    public string Description { get; set; } = Description;
+    public string? Description { get; set; } = Description;
 }
