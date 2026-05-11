c72d9c24daecc2b0a127c15406cafecde7c3d721
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-12 00:08:27 +0300

    add UnauthorizedPacket

---
+++ Auth/UnauthorizedPacket.cs
@@ -0,0 +1,7 @@
+﻿using OnChat.Protocol;
+using OnChat.Protocol.Packets;
+
+namespace OnChat.Shared.Auth;
+
+[PacketId(PacketId.UnauthorizedPacket)]
+public record UnauthorizedPacket(Guid CorrelationId, string Description) : FailureResponse(CorrelationId, Description);
