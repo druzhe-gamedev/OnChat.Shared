cd16920373092d41125dccdde7a75e084378e86d
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-12 01:41:46 +0300

    change message packets

---
+++ Messages/ReceiveMessagePacket.cs
@@ -0,0 +1,8 @@
+﻿using OnChat.Protocol;
+using OnChat.Protocol.Packets;
+using OnChat.Shared.Encryption;
+
+namespace OnChat.Shared.Messages;
+
+[PacketId(PacketId.ReceiveMessagePacket)]
+public record ReceiveMessagePacket(Guid CorrelationId, Guid SenderId, EncryptedMessage Message) : SuccessfulResponse(CorrelationId);
--- Messages/SendMessagePacket.cs
+++ Messages/SendMessagePacket.cs
@@ -1,7 +1,8 @@
 ﻿using OnChat.Protocol;
 using OnChat.Protocol.Packets;
+using OnChat.Shared.Encryption;
 
 namespace OnChat.Shared.Messages;
 
 [PacketId(PacketId.SendMessagePacket)]
-public record SendMessagePacket(Guid CorrelationId, string Token, Guid ReceiverId, string Message) : AuthenticatedPacket(CorrelationId, Token);
+public record SendMessagePacket(Guid CorrelationId, string Token, Guid ReceiverId, EncryptedMessage EncryptedMessage) : AuthenticatedPacket(CorrelationId, Token);
