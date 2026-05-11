0a231d0a6b6025563b4c8cfa6d6a0895d53fad36
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-11 21:19:21 +0300

    add token rotation packets

---
+++ Auth/BadRefreshToken.cs
@@ -0,0 +1,7 @@
+﻿using OnChat.Protocol;
+using OnChat.Protocol.Packets;
+
+namespace OnChat.Shared.Auth;
+
+[PacketId(PacketId.BadRefreshTokenPacket)]
+public record BadRefreshToken(Guid CorrelationId, string? Description) : FailureResponse(CorrelationId, Description);
---
+++ Auth/TokensRotationPacket.cs
@@ -0,0 +1,7 @@
+﻿using OnChat.Protocol;
+using OnChat.Protocol.Packets;
+
+namespace OnChat.Shared.Auth;
+
+[PacketId(PacketId.TokensRotationPacket)]
+public record TokensRotationPacket(Guid CorrelationId, string RefreshToken) : BasePacket(CorrelationId);
