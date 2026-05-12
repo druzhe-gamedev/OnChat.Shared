9116f5a068e614dc57b41bb5d5112d1d773134c5
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-12 18:40:04 +0300

    add PublicKeyPacket

---
+++ Encryption/PublicKeyPacket.cs
@@ -0,0 +1,4 @@
+﻿namespace OnChat.Shared.Encryption;
+
+[PacketId(PacketId.PublicKeyPacket)]
+public record PublicKeyPacket(Guid CorrelationId, string Token, byte[] Value) : AuthenticatedPacket(CorrelationId, Token);
