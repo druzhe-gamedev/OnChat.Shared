ac9c5e339dc4cd5dd8fda68efdb557e2ee97c22a
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-12 18:14:11 +0300

    add WrongIdPacket

---
+++ Messages/WrongIdPacket.cs
@@ -0,0 +1,7 @@
+﻿using OnChat.Protocol;
+using OnChat.Protocol.Packets;
+
+namespace OnChat.Shared.Messages;
+
+[PacketId(PacketId.SendMessagePacket)]
+public record WrongIdPacket(Guid CorrelationId, string Description) : FailureResponse(CorrelationId, Description);
