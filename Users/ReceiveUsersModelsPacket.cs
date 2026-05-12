8f8451fc56837589c1631b0c6212f839e66c0dc3
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-13 01:43:51 +0300

    rename

---
+++ Users/ReceiveUsersModelsPacket.cs
@@ -0,0 +1,6 @@
+﻿using OnChat.Protocol.Packets;
+
+namespace OnChat.Shared.Users;
+
+[PacketId(PacketId.ReceiveUsersModelsPacket)]
+public record ReceiveUsersModelsPacket(Guid CorrelationId, UserModel[] UsersModels) : SuccessfulResponse(CorrelationId);
--- Users/ReceiverUsersModelsPacket.cs
+++
@@ -1,6 +0,0 @@
-﻿using OnChat.Protocol.Packets;
-
-namespace OnChat.Shared.Users;
-
-[PacketId(PacketId.ReceiverUsersModelsPacket)]
-public record ReceiverUsersModelsPacket(Guid CorrelationId, UserModel[] UsersModels) : SuccessfulResponse(CorrelationId);
