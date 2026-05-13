72bced868b08261974677af1236fe0575006d9c1
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-13 22:43:46 +0300

    add SuccessfulPacket

---
+++ SuccessfulPacket.cs
@@ -0,0 +1,6 @@
+﻿using OnChat.Protocol.Packets;
+
+namespace OnChat.Shared;
+
+[PacketId(PacketId.SuccessfulPacket)]
+public record SuccessfulPacket(Guid CorrelationId, string? Description = "") : SuccessfulResponse(CorrelationId, Description);
