f2f68bf9d138bc358cb523c209c58c743ce4438c
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-14 21:50:51 +0300

    add MailConstraint

---
+++ Validation/MailConstraint.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace OnChat.Shared.Validation;
+
+public class MailConstraint<T>(Regex regex) : IConstraint<string> where T : INumber<T>
+{
+    public bool IsValid(string value) => regex.IsMatch(value);
+
+    public string GetErrorMessage(string? field) => $"Wrong email format{(string.IsNullOrEmpty(field) ? "" : $" for {field}")}";
+}
