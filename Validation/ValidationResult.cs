1734136a3371d8735cc751353a623a2992111bf5
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-14 22:26:31 +0300

    add TError to ValidationResultError

--- Validation/ValidationResult.cs
+++ Validation/ValidationResult.cs
@@ -4,16 +4,17 @@ public record ValidationResult;
 
 public record SuccessfulValidationResult : ValidationResult;
 
-public record ValidationResultError(string Description) : ValidationResult;
+public record ValidationResultError<TError>(string Description, TError ErrorCode) : ValidationResult;
 
 public static class ValidationResultExtension
 {
     extension(ValidationResult)
     {
         public static SuccessfulValidationResult Success() => new();
-        public static ValidationResultError Error(string description) => new(description);
 
-        public static ValidationResultError Error<T>(IConstraint<T> constraint, string field) =>
-            new(constraint.GetErrorMessage(field));
+        public static ValidationResultError<TError> Error<T, TError>(IConstraint<T> constraint, string field, TError errorCode) =>
+            new(constraint.GetErrorMessage(field), errorCode);
+
+        public static ValidationResultError<TError> Error<TError>(string description, TError errorCode) => new(description, errorCode);
     }
 }
