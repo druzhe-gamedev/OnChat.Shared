499bd02a31c6ff53de4b8143300c15d734d28355
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-14 16:01:17 +0300

    add RegistrationValidator

---
+++ Auth/Validation/RegistrationValidator.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+using OnChat.Shared.Validation;
+
+namespace OnChat.Shared.Auth.Validation;
+
+public partial class RegistrationValidator : ValidatorBase<RegistrationPacket>
+{
+    public static readonly LengthConstraint PasswordConstraint = new (new MinMaxValue<int>(5, 255));
+    public static readonly LengthRegexConstraint<byte> LoginConstraint = new (new MinMaxValue<byte>(3, 50), LoginRegex());
+    public static readonly LengthRegexConstraint<byte> MailConstraint = new (new MinMaxValue<byte>(3, 255), MailRegex());
+
+    [GeneratedRegex("^[a-zA-Z0-9][a-zA-Z0-9._-]{2,47}[a-zA-Z0-9]$")]
+    private static partial Regex LoginRegex();
+    
+
+    [GeneratedRegex("^(?=.{1,254}$)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")]
+    private static partial Regex MailRegex();
+
+    public RegistrationValidator()
+    {
+        AddConstraint(packet => packet.Password, PasswordConstraint);
+        AddConstraint(packet => packet.Email, MailConstraint);
+        AddConstraint(packet => packet.Username, LoginConstraint);
+        AddConstraint(packet => packet.Age, new MinMaxConstraint<short>(new MinMaxValue<short>(14, 99)));
+    }
+}
