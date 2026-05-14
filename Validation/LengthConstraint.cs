330fc24c6ab6b2ce9bceacabe20e45324a8f8338
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-14 21:58:06 +0300

    fix LengthConstraint

--- Validation/LengthConstraint.cs
+++ Validation/LengthConstraint.cs
@@ -2,7 +2,7 @@
 
 public class LengthConstraint(MinMaxValue<int> length) : IConstraint<string>
 {
-    public bool IsValid(string value) => value.Length < length.Min && value.Length > length.Max;
+    public bool IsValid(string value) => value.Length >= length.Min && value.Length <= length.Max;
     
     public string GetErrorMessage(string field) => $"{field} must be between {length.ToString("and")}";
 }
