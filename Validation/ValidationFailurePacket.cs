43ff4a9941fa18b0d7266beb7c5d06b2e1c75963
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-14 23:02:42 +0300

    add ValidationFailurePacket

---
+++ Validation/ValidationFailurePacket.cs
@@ -0,0 +1,6 @@
+﻿using OnChat.Protocol;
+using OnChat.Protocol.Packets;
+
+namespace OnChat.Shared.Validation;
+[PacketId(PacketId.ValidationFailurePacket)]
+public record ValidationFailurePacket(Guid CorrelationId, string? Description = "") : FailureResponse(CorrelationId, Description);
