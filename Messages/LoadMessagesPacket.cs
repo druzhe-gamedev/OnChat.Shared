8cc01b8c11c3de62da3d41dc65272f93f932f905
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-15 13:30:21 +0300

    add LoadMessagesPacket

---
+++ Messages/LoadMessagesPacket.cs
@@ -0,0 +1,7 @@
+﻿using OnChat.Protocol;
+using OnChat.Protocol.Packets;
+
+namespace OnChat.Shared.Messages;
+
+[PacketId(PacketId.LoadMessagesPacket)]
+public record LoadMessagesPacket(Guid CorrelationId, string Token, Guid ChatParticipant, int Quantity, int Page) : AuthenticatedPacket(CorrelationId, Token);
