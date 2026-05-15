25172ec908893d3870ac90fb92c8ab5a77137a00
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-15 13:30:27 +0300

    add WrongLimitsPacket

---
+++ Validation/WrongLimitsPacket.cs
@@ -0,0 +1,7 @@
+﻿using OnChat.Protocol;
+using OnChat.Protocol.Packets;
+
+namespace OnChat.Shared.Validation;
+
+[PacketId(PacketId.WrongLimitsPacket)]
+public record WrongLimitsPacket(Guid CorrelationId, string? Description = "") : FailureResponse(CorrelationId, Description);
