7f3b30a3569a808cdd024b504e4a9d7113f6499d
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-15 14:55:00 +0300

    add LoadMessagesValidator

---
+++ Messages/Validation/LoadMessagesValidator.cs
@@ -0,0 +1,14 @@
+﻿using OnChat.Shared.Validation;
+using OnChat.Protocol.Packets;
+
+namespace OnChat.Shared.Messages.Validation;
+
+public class LoadMessagesValidator : ValidatorBase<LoadMessagesPacket, PacketId>
+{
+    public readonly MinMaxConstraint<byte> QuantityConstraint = new(new MinMaxValue<byte>(0, 255));
+    
+    public LoadMessagesValidator()
+    {
+        AddConstraint(packet => packet.Quantity, QuantityConstraint, PacketId.WrongLimitsPacket);
+    }
+}
