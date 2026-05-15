2229da7cd7f3056f60a8661cfa61244e4bfd0db1
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-15 15:39:26 +0300

    add MessagesPacket

---
+++ Messages/MessagesPacket.cs
@@ -0,0 +1,8 @@
+﻿using OnChat.Protocol;
+using OnChat.Protocol.Packets;
+using OnChat.Shared.Encryption;
+
+namespace OnChat.Shared.Messages;
+
+[PacketId(PacketId.MessagesPacket)]
+public record MessagesPacket(Guid CorrelationId, EncryptedMessage[] Messages) : SuccessfulPacket(CorrelationId);
