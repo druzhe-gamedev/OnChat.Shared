88993708b9e7f81e9fda963b877b15f41c8e33ce
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-15 15:39:14 +0300

    add timestamp

--- Encryption/EncryptedMessage.cs
+++ Encryption/EncryptedMessage.cs
@@ -1,3 +1,3 @@
 ﻿namespace OnChat.Shared.Encryption;
 
-public record EncryptedMessage(byte[] EphemeralPublicKey, byte[] Nonce, byte[] Ciphertext, byte[] Tag);
+public record EncryptedMessage(byte[] EphemeralPublicKey, byte[] Nonce, byte[] Ciphertext, byte[] Tag, DateTimeOffset? TimeStamp);
