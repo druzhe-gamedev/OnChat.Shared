d209620de540bb3d5cbe824e3959a0f5deeb691f
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-15 15:00:51 +0300

    change constraint type

--- Messages/Validation/LoadMessagesValidator.cs
+++ Messages/Validation/LoadMessagesValidator.cs
@@ -5,7 +5,7 @@ namespace OnChat.Shared.Messages.Validation;
 
 public class LoadMessagesValidator : ValidatorBase<LoadMessagesPacket, PacketId>
 {
-    public readonly MinMaxConstraint<byte> QuantityConstraint = new(new MinMaxValue<byte>(0, 255));
+    public readonly MinMaxConstraint<int> QuantityConstraint = new(new MinMaxValue<int>(0, 255));
     
     public LoadMessagesValidator()
     {
