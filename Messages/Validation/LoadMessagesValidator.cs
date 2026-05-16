ef56765f690ef3bc383c5544155fde84b876e2e5
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-17 01:25:23 +0300

    add page constraint

--- Messages/Validation/LoadMessagesValidator.cs
+++ Messages/Validation/LoadMessagesValidator.cs
@@ -6,9 +6,11 @@ namespace OnChat.Shared.Messages.Validation;
 public class LoadMessagesValidator : ValidatorBase<LoadMessagesPacket, PacketId>
 {
     public readonly MinMaxConstraint<int> QuantityConstraint = new(new MinMaxValue<int>(0, 255));
+    public readonly MinMaxConstraint<int> PageConstraint = new(new MinMaxValue<int>(0, int.MaxValue));
     
     public LoadMessagesValidator()
     {
         AddConstraint(packet => packet.Quantity, QuantityConstraint, PacketId.WrongLimitsPacket);
+        AddConstraint(packet => packet.Page, PageConstraint, PacketId.WrongLimitsPacket);
     }
 }
