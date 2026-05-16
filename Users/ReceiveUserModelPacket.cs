bfb47ac11c90ea63b87eb9d89bf12757383fd50a
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-17 00:35:28 +0300

    add ReceiveUserModelPacket

---
+++ Users/ReceiveUserModelPacket.cs
@@ -0,0 +1,6 @@
+﻿using OnChat.Protocol.Packets;
+
+namespace OnChat.Shared.Users;
+
+[PacketId(PacketId.ReceiveUserModelPacket)]
+public record ReceiveUserModelPacket(Guid CorrelationId, UserModel UserModel) : SuccessfulResponse(CorrelationId);
