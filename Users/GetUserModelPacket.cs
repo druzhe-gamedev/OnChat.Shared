c4c98522664e42447b8a201d639eba282abfc9d2
Druzhe <57436324+druzhe-gamedev@users.noreply.github.com>
2026-05-17 00:27:11 +0300

    add GetUserModelPacket

---
+++ Users/GetUserModelPacket.cs
@@ -0,0 +1,6 @@
+﻿using OnChat.Protocol.Packets;
+
+namespace OnChat.Shared.Users;
+
+[PacketId(PacketId.GetUserModelPacket)]
+public record GetUserModelPacket(Guid CorrelationId, Guid UserId) : BasePacket(CorrelationId);
